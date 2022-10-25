9f5c4847e84b029c4def92f67249a4a8bfadd35f
Victor França <vito.daleste@gmail.com>
2022-10-25 09:12:12 -0300

    feat: add providers and controllers in module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { TodoController } from "./controllers/todo.controller";
 import { Todo } from "./entities/todo.entity";
+import { TodoService } from "./services/todo.service";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Todo])],
-    providers: [],
-    controllers: [],
+    providers: [TodoService],
+    controllers: [TodoController],
     exports: [TypeOrmModule]
 })
 
